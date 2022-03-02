94bcdee60ebfb9c9dcc17eb23561f02d354fd16a
Alan Kelly <alankelly@gmail.com>
2022-03-02 21:25:21 +0000

    basic functionality complete

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
-fetch ("https://apis.scrimba.com/jsonplaceholder/posts")
-    .then (res => res.json())
-    .then (data => {
-        const postsArr = data.slice (0,5)
-        console.log(postsArr)
-        let html = ""
-        for (let post of postsArr){
-            html+=
-            `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `
-        }
-            document.getElementById("posts").innerHTML = html
-        
+let postsArray = []
+
+function renderPosts() {
+    let html = ""
+    for (let post of postsArray) {
+        html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `
+    }
+    document.getElementById("posts").innerHTML = html
+    
+}
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data => {
+        postsArray = data.slice(0, 5)
+        renderPosts()
     })
 
-document.getElementById("new-post").addEventListener("submit", function(e){
+const newPostForm = document.getElementById("new-post")
+newPostForm.addEventListener("submit", function(e) {
     e.preventDefault()
     const postTitle = document.getElementById("post-title").value
     const postBody = document.getElementById("post-body").value
     const data = {
         title: postTitle,
         body: postBody
-    }  
-    console.log(postTitle)
+    }
+    newPostForm.reset()
+
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(post => {
+            postsArray.unshift(post)
+            renderPosts()
+
+
+        })
+       
 })
